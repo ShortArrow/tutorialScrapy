070d8e8cfa6ba053439897d98200d85cbade3ed8
shortarrow <bamboogeneral@live.jp>
2021-04-22 10:59:05 +0900

    fin

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        atag = response.css('li.next a').get()
+        if atag is not None:
+            yield response.follow(atag, callback=self.parse)
